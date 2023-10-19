b6e500ac3d5ca9858e05b6da34bff69e37b2ee40
Michel Storms <stormychel@me.com>
2023-10-19 22:46:41 +0200

    - example showing error on macOS Sonoma

--- YouTubePlayerKitTest/ContentView.swift
+++ YouTubePlayerKitTest/ContentView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import YouTubePlayerKit
 
 struct ContentView: View {
+    @StateObject var youTubePlayer: YouTubePlayer = "https://youtube.com/watch?v=psL_5RIBqnY"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("Hello, world!")
+            
+            YouTubePlayerView(self.youTubePlayer) { state in
+                // Overlay ViewBuilder closure to place an overlay View
+                // for the current `YouTubePlayer.State`
+                switch state {
+                case .idle:
+                    ProgressView()
+                case .ready:
+                    EmptyView()
+                case .error(let error):
+                    Text(verbatim: "YouTube player couldn't be loaded with error: \(error)")
+                }
+            }
         }
         .padding()
     }
