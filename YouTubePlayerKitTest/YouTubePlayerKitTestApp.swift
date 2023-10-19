6cceb19e9f927132614e4a3ad93001b1a586e9ab
Michel Storms <stormychel@me.com>
2023-10-19 22:38:11 +0200

    Initial Commit

---
+++ YouTubePlayerKitTest/YouTubePlayerKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubePlayerKitTestApp.swift
+//  YouTubePlayerKitTest
+//
+//  Created by Michel Storms on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct YouTubePlayerKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
